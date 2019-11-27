a8c1767d47c587cd20b7392e8d9830a32d1b16e8
hieukut456 <trunghieu9946@gmail.com>
2019-11-28 00:08:18 +0700

    Create account html

--- Presentation/wwwroot/js/main.js
+++ Presentation/wwwroot/js/main.js
@@ -127,7 +127,7 @@
  		$this.find('> a').attr('aria-expanded', false);
  		// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
  		$this.find('.dropdown-menu').removeClass('show');
- 		// }, 100);
+ 		// }, 100);~
  	});
 
 
@@ -314,27 +314,32 @@
 		mainClass: 'mfp-fade',
 		removalDelay: 160,
 		preloader: false,
-
- 		fixedContentPos: false
- 	});
- 	$('.checkin_date').datepicker({
- 		'format': 'dd/mm/yyyy',
- 		'todayHighlight': 'true',
- 		'startDate': '0d',
- 		'autoclose': true
- 	});
- 	$('.checkout_date').datepicker({
+		fixedContentPos: false
+	});
+	// DatePicker
+	$('.checkin_date').datepicker({
+		'format': 'dd/mm/yyyy',
+		'todayHighlight': 'true',
+		'startDate': '0d',	
+		'autoclose': true
+	});
+	$('.checkout_date').datepicker({
 		'format': 'dd/mm/yyyy',
 		'setDate': $('.checkin_date').val(),
 		'startDate': '0d',
 		'autoclose': true
 	});
-
- 	function addUser() {
- 		$("#register").dialog("close");
- 		alert("Succesful");
- 	}
- 	$('.checkin_date').datepicker('setDate', new Date());
+	$('.checkin_date').datepicker('setDate', new Date());
+	$(".birth_date").datepicker({
+		'format': 'dd/mm/yyyy',
+		'autoclose': true
+	})
+	//end of datepicker
+	function addUser() {
+		$("#register").dialog("close");
+		alert("Succesful");
+	}
+	//dialog,form
  	var dialog, form;
  	$("#register").dialog({
  		autoOpen: false,
@@ -389,35 +394,51 @@
  			}
  		});
  	});
- 	dialog = $("#dialog").dialog({
- 		autoOpen: false,
- 		height: 350,
- 		width: 320,
- 		maxHeight: 350,
- 		maxWidth: 320,
- 		resizable: false,
- 		modal: true,
- 		dialogClass: 'myTitleClass',
- 		position: 'center',
- 		buttons: {
- 			Register: {
- 				text: "Register",
- 				id: "open-register",
- 				click: function () {
- 					$("#register").dialog("open");
- 					$(this).dialog("close");
- 				}
- 			},
- 			Cancel: function () {
- 				$(this).dialog("close");
- 			}
- 		},
- 		close: function () {
+	dialog = $("#dialog").dialog({
+		autoOpen: false,
+		height: 350,	
+		width: 320,
+		maxHeight: 350,
+		maxWidth: 320,
+		resizable: false,
+		modal: true,
+		dialogClass: 'myTitleClass',
+		position: 'center',	
+		buttons: {
+			Register: {
+				text: "Register",
+				id: "open-register",
+				click: function () {
+					$("#register").dialog("open");
+					$(this).dialog("close");
+				}
+			},
+			Cancel: function () {
+				$(this).dialog("close");
+			}
+			},
+			close: function () {
  			//form[0].reset();
- 			$(this).dialog("close")
+			$(this).dialog("close")
 
- 		}
- 	});
+		}
+	});
+	$("#opener").on("click", function () {
+		$("#dialog").dialog("open");
+	});
+	
+	$("#dialog").dialog("option", "closeOnEscape", true);
+	$(".info-show").on("click", function () {
+		$(".info-show").css("display", "none");
+		$(".info-hide").css("display", "inline-block");
+		$(".fare__details").css("display", "block");
+	});
+	$(".info-hide").on("click", function () {
+		$(".info-hide").css("display", "none");
+		$(".info-show").css("display", "inline-block");
+		$(".fare__details").css("display", "none");
+	})
+	//end of dialog
  	$(window).resize(function () {
  		$("#dialog").dialog("option", "position", {
  			my: "center",
@@ -430,20 +451,7 @@
  			of: window
  		});
  	});
- 	$("#opener").on("click", function () {
- 		$("#dialog").dialog("open");
- 	});
- 	$("#dialog").dialog("option", "closeOnEscape", true);
- 	$(".info-show").on("click", function () {
- 		$(".info-show").css("display", "none");
- 		$(".info-hide").css("display", "inline-block");
- 		$(".fare__details").css("display", "block");
- 	});
- 	$(".info-hide").on("click", function () {
- 		$(".info-hide").css("display", "none");
- 		$(".info-show").css("display", "inline-block");
- 		$(".fare__details").css("display", "none");
- 	})
+ 	
 
  	//Autocomplete
  	$("#From").autocomplete({
@@ -452,7 +460,107 @@
  	$("#Where").autocomplete({
 		source: '/Index?handler=AirPort'
 	})
-	$("#setting").on("click",function(){
-		$(".content").html('<div style="padding-left:50px;height: 1073px">OK</div>');
+	// End autocomplete
+	//Open close class
+	$(".open-user").on("click",function(){
+		$(".user-show").addClass("hidden");
+		$(".user-field").removeClass("hidden");
+		$(".open-user").addClass("hidden");
+		$(".close-user").removeClass("hidden");
+		$(".save-user").removeClass("hidden");
 	});
+	$(".open-pass").on("click",function(){
+		$("#pass-field").removeClass("hidden");
+		$("#pass-show").addClass("hidden");
+		$(".open-pass").addClass("hidden");
+		$(".close-pass").removeClass("hidden");
+		$(".save-pass").removeClass("hidden");
+	});
+	$(".close-user").on("click",function(){
+		$(".user-show").removeClass("hidden");
+		$(".user-field").addClass("hidden");
+		$(".open-user").removeClass("hidden");
+		$(".close-user").addClass("hidden");
+		$(".save-user").addClass("hidden");
+	});
+	$(".close-pass").on("click",function()
+	{
+		$("#pass-field").addClass("hidden");
+		$("#pass-show").removeClass("hidedn");
+		$(".open-pass").removeClass("hidden");
+		$(".close-pass").addClass("hidden");
+		$(".save-pass").addClass("hidden");
+	});
+	$(".open").on("click",function(){
+		var id = $(this).attr("id");
+		if(id == "profile_op")
+		{
+			$("#profile_op").addClass("hidden");
+			$("#profile_cl").removeClass("hidden");
+			$("#profile_s").removeClass("hidden");
+			$(".pro-show").addClass("hidden");
+			$(".pro-field").removeClass("hidden");
+		}
+		if(id == "add_op")
+		{
+			$("#add_op").addClass("hidden");
+			$("#add_cl").removeClass("hidden");
+			$("#add_s").removeClass("hidden");
+			$("#address-show").addClass("hidden");
+			$("#address-field").removeClass("hidden");
+		}
+		if(id == "phone_op")
+		{
+			$("#phone_op").addClass("hidden");
+			$("#phone_cl").removeClass("hidden");
+			$("#phone_s").removeClass("hidden");
+			$(".phone-show").addClass("hidden");
+			$(".phone-field").removeClass("hidden");
+		}
+	});
+	$(".cancel").on("click",function(){
+		var id = $(this).attr("id");
+		if(id == "profile_cl")
+		{
+			$("#profile_op").removeClass("hidden");
+			$("#profile_cl").addClass("hidden");
+			$("#profile_s").addClass("hidden");
+			$(".pro-show").removeClass("hidden");
+			$(".pro-field").addClass("hidden");
+		}
+		if(id == "add_cl")
+		{
+			$("#add_op").removeClass("hidden");
+			$("#add_cl").addClass("hidden");
+			$("#add_s").addClass("hidden");
+			$("#address-show").removeClass("hidden");
+			$("#address-field").addClass("hidden");
+		}
+		if(id == "phone_cl")
+		{
+			$("#phone_op").removeClass("hidden");
+			$("#phone_cl").addClass("hidden");
+			$("#phone_s").addClass("hidden");
+			$(".phone-show").removeClass("hidden");
+			$(".phone-field").addClass("hidden");
+		}
+	});
+	//end of open close class
+
+	// select country
+	var country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+	var option = '';
+	for(var i = 0;i<country_list.length;i++)
+	{
+		option += '<option value="' + country_list[i] + '">' + country_list[i] + '</option>';
+	}
+	$("#items").append(option);
+	//end of array coutries
+	
+	//test
+	var firstName = $("#firstName").val();
+	var lastName = $("#lastName").val();
+	$(".name").text(firstName+" "+lastName);
+	var birth_date = $(".birth_date").val();
+	$(".birth_day").text(birth_date);
 })(jQuery);
