70b4dbe23eef6fa1a5481ed78d2f510dcbef13a0
hieukut456 <trunghieu9946@gmail.com>
2019-11-30 23:35:21 +0700

    fix

--- Presentation/Services/ServiceInterfaces/IFlightVMService.cs
+++ Presentation/Services/ServiceInterfaces/IFlightVMService.cs
@@ -7,6 +7,6 @@ namespace Presentation.Services.ServiceInterfaces
 {
     public interface IFlightVMService
     {
-        Task<IEnumerable<FlightDTO>> GetFlightListVm(string origin_id, string destination_id, DateTime dep_date, DateTime arr_date, int adults_num, int childs_num);
+        // Task<IEnumerable<FlightDTO>> GetFlightListVm(string origin_id, string destination_id, DateTime dep_date, DateTime arr_date, int adults_num, int childs_num);
     }
 }
