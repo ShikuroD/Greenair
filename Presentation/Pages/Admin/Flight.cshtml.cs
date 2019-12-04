2c53c45ad8415a022f2d246e8f18e08c32a9c5fd
TruongGiangVu <45998623+TruongGiangVu@users.noreply.github.com>
2019-12-04 07:52:50 +0700

    zx

--- Presentation/Pages/Admin/Flight.cshtml.cs
+++ Presentation/Pages/Admin/Flight.cshtml.cs
@@ -28,11 +28,14 @@ namespace Presentation.Pages.Admin
         public IEnumerable<FlightDTO> ListFlights { get; set; }
         public IEnumerable<Plane> ListPlanes { get; set; }
         public IEnumerable<Route> ListRoutes { get; set; }
+        public IEnumerable<Airport> ListAirports { get; set; }
+        public IEnumerable<FlightDetail> FlightDetail { get; set; }
         public async Task OnGet()
         {
             ListFlights = await _services.getAllFlightAsync();
             ListPlanes = await _unitofwork.Planes.GetAllAsync();
             ListRoutes = await _unitofwork.Routes.GetAllAsync();
+            ListAirports = await _unitofwork.Airports.GetAllAsync();
         }
     }
 }
