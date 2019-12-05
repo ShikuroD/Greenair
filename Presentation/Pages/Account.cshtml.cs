1eb7e866e4256dedd97e2643cb1b35185da2789c
hieukut456 <trunghieu9946@gmail.com>
2019-12-05 22:21:23 +0700

    Fix login

--- Presentation/Pages/Account.cshtml.cs
+++ Presentation/Pages/Account.cshtml.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Presentation.Helpers;
+using Presentation.Services.ServiceInterfaces;
 
 namespace Presentation.Pages
 {
@@ -20,17 +21,51 @@ namespace Presentation.Pages
     {
         private readonly IAccountService _accountService;
         [ActivatorUtilitiesConstructor]
-        public AccountModel(IAccountService accountService)
+        private readonly ICustomerService _customerService;
+        public AccountModel(IAccountService accountService, ICustomerService customerService)
         {
             _accountService = accountService;
+            _customerService = customerService;
         }
-        private readonly ILogger<AccountModel> _logger;
+        // private readonly ILogger<AccountModel> _logger;
         public string Msg { get; set; }
-        public AccountModel(ILogger<AccountModel> logger)
-        {
-            _logger = logger;
-        }
+        // public AccountModel(ILogger<AccountModel> logger)
+        // {
+        //     _logger = logger;
+        // }
 
+        public IActionResult OnGetUserField()
+        {
+            
+            string userId = "";
+            string check = "";
+            if( HttpContext.Session.GetString("username") != null)
+            {                
+                // html+="<div class='dropdown user nav-link'>";
+                // html+="<a id='account-dropdown' class=' dropdown-toggle ' style='cursor:pointer' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false' >";
+                // html+="<span class='ion-ios-person' ></span>";
+                // html+="<span style='padding-left:10px'>"+ userId +"</span>";
+                // html+="</a>";
+                // html+="<div class='dropdown-menu' aria-labelledby='account-dropdown'>";
+                
+                // html+="<button class='dropdown-item' type='button'>Your Profile</button>";
+                // html+="<button id='logOut' class='dropdown-item'  type='button'>Logout</button>";
+                // html+="</div>";
+                // html+="</div>";
+                check = "not"; 
+                userId = HttpContext.Session.GetString("username");
+
+            }
+            else{   
+                // html+="<span class='ion-ios-person' ></span>";
+                // html+="<span style='padding-left:10px'>Log in</span>";
+                check = "null";
+            }
+            Dictionary<string,string> Results = new Dictionary<string, string>();
+            Results.Add("userid",userId);
+            Results.Add("check",check);
+            return new JsonResult(Results);
+        }
         public void OnGet()
         {
 
@@ -39,6 +74,7 @@ namespace Presentation.Pages
         {
             string username = "";
             string password = "";
+            string userId = "";
             {
                 MemoryStream stream = new MemoryStream();
                 Request.Body.CopyTo(stream);
@@ -56,7 +92,11 @@ namespace Presentation.Pages
                             AccountDTO account = new AccountDTO(username,password);
                             if(await _accountService.loginCheckAsync(account))
                             {
-                                HttpContext.Session.SetString("username",username);
+                                
+                                Person Person = await _accountService.getPersonByAccount(username);
+                                CustomerDTO customer = await _customerService.getCustomerAsync(Person.Id.ToString());
+                                userId = customer.FirstName;
+                                HttpContext.Session.SetString("username",userId);
                                 Msg = "true";
                             }
                             else{
@@ -67,9 +107,15 @@ namespace Presentation.Pages
                 }
             }
             Dictionary<string, string> lstString = new Dictionary<string, string>();
-            lstString.Add("username", username);
+            lstString.Add("userid", userId);
             lstString.Add("msg", Msg);
             return new JsonResult(lstString);
         }
+        public IActionResult OnPostLogout()
+        {
+            HttpContext.Session.Remove("username");
+            return new JsonResult(Msg);
+        }
+
     }
 }
