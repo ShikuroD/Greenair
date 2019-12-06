d365c8e042d36e3a1790804f8ae0e27e024e531d
ShikuroD laptop <robotboy.com.vn@gmail.com>
2019-12-06 08:31:08 +0700

    fix

---
+++ ApplicationCore/Services/IEmployeeService.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using System;
+using ApplicationCore.DTOs;
+using ApplicationCore.Entities;
+using System.Collections.Generic;
+namespace ApplicationCore.Services
+{
+    public interface IEmployeeService : IService<Employee, EmployeeDTO, EmployeeDTO>
+    {
+        //query
+        Task<EmployeeDTO> getEmployeeAsync(string emp_id);
+        Task<IEnumerable<EmployeeDTO>> getAllEmployeeAsync();
+        Task<IEnumerable<EmployeeDTO>> getEmployeeByJobAsync();
+        Task<IEnumerable<EmployeeDTO>> getEmployeeByName(string lastname, string firstname);
+        Task<IEnumerable<EmployeeDTO>> getEmployeeByName(string fullname);
+
+        //actions
+
+        Task addEmployeeAsync(EmployeeDTO dto);
+        Task removeEmployeeAsync(string emp_id);
+        Task updateEmployeeAsync(EmployeeDTO dto);
+
+        Task createAccountAsync(AccountDTO acc_dto);
+        Task updateAccountAsync(AccountDTO acc_dto);
+
+        Task disableCutomerAsync(string emp_id);
+        Task activateEmployeeAsync(string emp_id);
+    }
+}
